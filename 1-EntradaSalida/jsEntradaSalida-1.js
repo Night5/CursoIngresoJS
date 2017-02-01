4d45c669dc1f364bbe084fdf1a9937c3dce4c236
Night5 <aldy_11_08@hotmail.com>
2017-02-01 12:25:48 -0300

    Algo

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,8 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostar()
 {
+	prompt("Esto funciona de maravilla") 
 	
+
 }
 
--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,7 +2,9 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
+	var edad = prompt ("Mostrar el nombre")
+	alert(edad)
+fdgg
 }
 
 
