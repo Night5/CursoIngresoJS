9f7140c70df360fb7048c41de56eb5b3f7c29216
Night5 <aldy_11_08@hotmail.com>
2017-02-02 09:52:45 -0300

    Ejercicio 3
    Modificado.

---
+++ jsEntradaSalida-3.js
@@ -0,0 +1,19 @@
+/*Debemos lograr tomar un dato por 'ID'
+y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+	var nombre = document.getElementById("elNombre").value;
+	alert("El nombre ingresao es: " + nombre);
+
+	/*Creamos una variable, obtenemos el valor por ID y 
+	lo guardamos en dicha variable.
+	Luego lo mostramos por Alert.
+
+
+	*/ 
+ 
+
+
+}
+
+
