79a910bad57006ed13e3fc9080625028a07c4230
Night5 <aldy_11_08@hotmail.com>
2017-02-02 12:08:06 -0300

    Ejercicio 9
    Finalizado.

---
+++ jsEntradaSalida-9.js
@@ -0,0 +1,13 @@
+/*Debemos lograr tomar el importe por ID ,
+transformarlo a entero (parseInt), luego
+mostrar el importe con un aumento del 10 %
+en el cuadro de texto "RESULTADO".*/
+function MostrarAumento()
+{
+	var sueldo = parseInt(document.getElementById("sueldo").value);
+	var aumento = (sueldo * 10) / 100;
+	var resultadoFinal = sueldo + aumento;
+	document.getElementById("resultado").value = resultadoFinal;
+
+	
+}
