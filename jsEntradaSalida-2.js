9f5e9976676d5c643cb9ab83d7d839ea83d183cb
Night5 <aldy_11_08@hotmail.com>
2017-02-02 09:55:00 -0300

    Ejercicio 2
    Modificado,

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,16 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+	var a;
+	a = prompt("Ingrese un nùmero;");
+	alert(a); 
+	
+
+
+
+
+
+
+}
+
